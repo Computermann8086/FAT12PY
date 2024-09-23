e923f59d68863104a21a53f1f137651283a121bd
Computer Mann <112242931+Computermann8086@users.noreply.github.com>
2024-09-23 20:21:33 +0200

    Update FAT12.py

--- FAT12.py
+++ FAT12.py
@@ -1,7 +1,8 @@
 class fat_obj:
-    def __init__(self):
-        self.fs_obj = []
-    def open_fat12(path):
-        with open(path, 'rb') as fat_fs:
+    def __init__(self, path):
+        self.path = path
+        self.fs_obj = ''
+    def open_fat12(self.path):
+        with open(self.path, 'rb') as fat_fs:
             fs_obj = fat_fs.read()
     
